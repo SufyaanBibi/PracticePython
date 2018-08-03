6b548bc43d8c339f935dcc8288e3e8c6ac4c85a6
Sufyaan <hannah@Aeryn.connect>
2018-08-03 12:14:42 +0100

    Modified tests to include the creation of the game board with 3.

--- CodingPractice/tests/test_GameBoard.py
+++ CodingPractice/tests/test_GameBoard.py
@@ -22,6 +22,16 @@ class GameBoardTests(unittest.TestCase):
         board_size = None
         self.assertEqual(None, horizontal_lines(board_size))
 
+    def test_three_horizontal_lines(self):
+        board_size = 3
+        expected_result = ' ---  ---  --- '
+        self.assertEqual(expected_result, horizontal_lines(board_size))
+
+    def test_three_vertical_lines(self):
+        board_size = 3
+        expected_result = '|    |    |    |    '
+        self.assertEqual(expected_result, vertical_lines(board_size))
+
 
 if __name__ == '__main__':
     unittest.main()
