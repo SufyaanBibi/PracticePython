cfa179de98065d5e79782d1705d1bf8aba6c8e6c
Sufyaan <hannah@Aeryn.connect>
2018-08-06 16:44:37 +0100

    Added new tests to Issue_1.

--- CodingPractice/excercises/RunGameBoard.py
+++ CodingPractice/excercises/RunGameBoard.py
@@ -1,10 +1,10 @@
-from GameBoard import horizontal_lines, vertical_lines
+from CodingPractice.excercises.GameBoard import horizontal_lines, vertical_lines
 
 board_size = int(input('Enter board size here: '))
 
 for element in range(board_size):
-    horizontal_lines()
-    vertical_lines()
+    horizontal_lines(board_size)
+    vertical_lines(board_size)
 
 
-horizontal_lines()
+print(horizontal_lines(board_size))
