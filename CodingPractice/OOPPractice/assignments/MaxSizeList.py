263c71a59142c006cdeee046c01a82e995d7fba2
Sufyaan <hannah@Aeryn.connect>
2018-09-06 11:17:16 +0100

    Refactored MaxSizeList.py to include double underscores as per suggestion.

--- CodingPractice/OOPPractice/assignments/MaxSizeList.py
+++ CodingPractice/OOPPractice/assignments/MaxSizeList.py
@@ -1,13 +1,13 @@
 class MaxSizeList:
 
     def __init__(self, limit):
-        self.limit = limit
-        self.inner_list = []
+        self.__limit = limit
+        self.__inner_list = []
 
     def push(self, value):
-        self.inner_list.append(value)
-        if len(self.inner_list) > self.limit:
-            self.inner_list.pop(0)
+        self.__inner_list.append(value)
+        if len(self.__inner_list) > self.__limit:
+            self.__inner_list.pop(0)
 
     def get_list(self):
-        return self.inner_list
+        return self.__inner_list
