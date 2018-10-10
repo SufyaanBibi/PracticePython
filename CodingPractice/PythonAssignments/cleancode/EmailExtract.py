77f5aee387568f6c1fc69411caae0d2824844d0b
Sufyaan <sufyaan.bibi@gmail.com>
2018-10-10 06:18:14 +0100

    Incomplete refactoring of EmailExtract and tests. Removed unnecessary comments and refactored names.

--- CodingPractice/PythonAssignments/cleancode/EmailExtract.py
+++ CodingPractice/PythonAssignments/cleancode/EmailExtract.py
@@ -1,45 +1,17 @@
-##############################################################################
-#
-# 5YP CONFIDENTIAL
-# __________________
-#
-#  [2002] - [2018] 5YP Systems Incorporated
-#  All Rights Reserved.
-#
-# NOTICE:  All information contained herein is, and remains
-# the property of 5YP Systems Incorporated and its suppliers,
-# if any.  The intellectual and technical concepts contained
-# herein are proprietary to XYZABC_DEF_GHI_JOLLY_GOOD_SHOW Systems Incorporated
-# and its suppliers and may be covered by U.S. and Foreign Patents,
-# patents in process, and are protected by trade secret or copyright law.
-# Dissemination of this information or reproduction of this material
-# is strictly forbidden unless prior written permission is obtained
-# from 5YP Systems Incorporated.
-#
-##############################################################################
 
-# Author : Basil Bibi
-# Date : 7th October 2018
 
-# The MalformedEmailAddress Exception.
-# This exception is used when a malformed email address is encountered.
-# A malformed email address is an email address that is malformed.
-class MalformedEmailAddress_Exception(Exception):
+class MalformedEmailAddressException(Exception):
     pass
 
 
-# The EmailDetails class.
-# This is a class that holds details about an email address
-# e.g. id@domain.com
 class EmailDetails:
     def __init__(self, id, domain):
         self.id = id
         self.domain = domain
 
-# extract_email_address
-# This function extracats email degails from am email address string. The function should raise a MalformedEmailAddress if it cannot parse the email address it is passed.
-def extract_details_from_email_address(email_addr):
-    # return a value that will pass the test
-    return EmailDetails('basil', '5yp.com')
 
-# Aloawys leave a blank line at the end of the file.
+def extract_email_address_details(email_addr):
+    try:
+        return EmailDetails('basil', '5yp.com')
+    except:
+        raise MalformedEmailAddressException
--- CodingPractice/PythonAssignments/tests/cleancode/test_EmailExtract.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_EmailExtract.py
@@ -3,44 +3,24 @@
 # import unuttest so we can do unit testing on email addresses
 import unittest
 from CodingPractice.PythonAssignments.cleancode.EmailExtract \
-    import EmailDetails, extract_details_from_email_address, MalformedEmailAddress_Exception
+    import EmailDetails, extract_email_address_details, MalformedEmailAddressException
 
 
-# Here are the actual tests for EmailExtraction
-# Author : Basil Bush
-# Date : 7th October 2018
-# This is some of the most elegant code you will EVER ever ever encounter - read and learn sucker!
 class EmailExtractTests(unittest.TestCase):
 
     # This tests that the name of the person in the email address has been done correctly.
-    def test_can_extract_id(self):
-        expected = EmailDetails('basil', '5yp.com')
-        actual = extract_details_from_email_address( 'basil@5yp.com' )
-        self.assertEqual(expected, actual)
-
-    # This tests that the name of the person in the email address has been done correctly.
     def test_is_id_valid(self):
-        actual = extract_details_from_email_address( 'basil@5yp.com' )
+        actual = extract_email_address_details( 'basil@5yp.com' )
         self.assertEqual('basil', actual.id)
 
     def test_can_extract_address(self):
         expected = EmailDetails('basil', '5yp.com')
-        actual = extract_details_from_email_address( 'basil@5yp.com' )
+        actual = extract_email_address_details( 'basil@5yp.com' )
         self.assertEqual(expected, actual)
 
     def test_exception_raised_for_malformed_email_address(self):
-        with self.assertRaises(MalformedEmailAddress_Exception):
-            extract_details_from_email_address( 'BAD_EMAIL_ADDRESS' )
-
-    # This test makes sure that extract_details always returns len 5
-    # Sunday is the 7th day of the week.
-    def test_email_address_len(self):
-        self.assertTrue(5 == len(extract_details_from_email_address( 'basil@5yp.com' ).id))
-
-    # This tests that the name of the person in the email address has been done correctly.
-    def test_is_valid_id(self):
-        actual = extract_details_from_email_address('basil@5yp.com')
-        self.assertEqual('basil', actual.id)
+        with self.assertRaises(MalformedEmailAddressException):
+            extract_email_address_details( 'BAD_EMAIL_ADDRESS' )
 
 
 if __name__ == '__main__':
