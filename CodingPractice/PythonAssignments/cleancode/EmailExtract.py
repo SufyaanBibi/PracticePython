7a3c2a226b09e7fb8cdb7c5c732b6d718871b935
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-05 11:31:06 +0000

    Added tests to EmailExtract.py.

--- CodingPractice/PythonAssignments/cleancode/EmailExtract.py
+++ CodingPractice/PythonAssignments/cleancode/EmailExtract.py
@@ -6,26 +6,30 @@ class MalformedEmailAddressException(Exception):
 
 
 class EmailDetails:
-    def __init__(self, id, domain):
-        self.id = id
-        self.domain = domain
+    def __init__(self, local_part, domain):
+        self._local_part = local_part
+        self._domain = domain
 
     def __eq__(self, other):
-        return self.id == other.id and self.domain == other.domain
+        return self._local_part == other._local_part and self._domain == other._domain
 
     def get_id(self):
-        return self.id
+        return self._local_part
 
     def get_domain(self):
-        return self.domain
+        return self._domain
 
 
 def extract_email_address_details(email_addr):
+    pattern = re.compile('[^@,]+@[^@,]+\.[^@,]+')
     try:
-        if not re.match(r'[^@,\s+]+@[^@,\s+]+\.[^@,\s+]+', email_addr):
-            raise MalformedEmailAddressException(email_addr)
-        email_details = email_addr.split('@')
-        email_id, domain = email_details
-        return EmailDetails(email_id, domain)
-    except:
+        if re.match(pattern, email_addr):
+            email_id, domain = email_addr.split('@')
+            return EmailDetails(email_id, domain)
+
+        raise MalformedEmailAddressException(email_addr)
+
+    except MalformedEmailAddressException:
+        raise
+    except Exception:
         raise MalformedEmailAddressException(email_addr)
--- CodingPractice/PythonAssignments/tests/cleancode/test_EmailExtract.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_EmailExtract.py
@@ -7,39 +7,94 @@ class EmailExtractTests(unittest.TestCase):
 
     def test_00_is_id_valid(self):
         actual = extract_email_address_details( 'basil@5yp.com' )
-        self.assertEqual('basil', actual.id)
+        self.assertEqual('basil', actual._local_part)
 
     def test_01_can_extract_address(self):
         expected = EmailDetails('basil', '5yp.com')
         actual = extract_email_address_details( 'basil@5yp.com' )
         self.assertEqual(expected, actual)
 
-    def test_02_exception_raised_for_malformed_email_address(self):
+    def test_02_valid_email_address(self):
+        expected = EmailDetails('simple', 'example.com')
+        actual = extract_email_address_details('simple@example.com')
+        self.assertEqual(expected, actual)
+
+    def test_03_valid_very_common_email_address(self):
+        expected = EmailDetails('very.common', 'example.co.uk')
+        actual = extract_email_address_details('very.common@example.co.uk')
+        self.assertEqual(expected, actual)
+
+    def test_04_valid_email_address_with_symbol(self):
+        expected = EmailDetails('disposable.style.email.with+symbol', 'example.com')
+        actual = extract_email_address_details('disposable.style.email.with+symbol@example.com')
+        self.assertEqual(expected, actual)
+
+    def test_05_valid_email_address_with_hyphen(self):
+        expected = EmailDetails('other.email-with-hyphen', 'example.co.uk')
+        actual = extract_email_address_details('other.email-with-hyphen@example.co.uk')
+        self.assertEqual(expected, actual)
+
+    def test_06_valid_email_address_fully_qualified_domain(self):
+        expected = EmailDetails('fully-qualified-domain', 'example.com')
+        actual = extract_email_address_details('fully-qualified-domain@example.com')
+        self.assertEqual(expected, actual)
+
+    def test_07_valid_email_address_with_tag_sorting(self):
+        expected = EmailDetails('user.name+tag+sorting', 'example.com')
+        actual = extract_email_address_details('user.name+tag+sorting@example.com')
+        self.assertEqual(expected, actual)
+
+    def test_08_valid_email_address_one_letter_local_part(self):
+        expected = EmailDetails('x', 'example.com')
+        actual = extract_email_address_details('x@example.com')
+        self.assertEqual(expected, actual)
+
+    def test_09_valid_email_address_strange_domain(self):
+        expected = EmailDetails('example-indeed', 'strange-example.com')
+        actual = extract_email_address_details('example-indeed@strange-example.com')
+        self.assertEqual(expected, actual)
+
+    def test_10_invalid_email_address_domain_name_no_TLD(self):
+        with self.assertRaises(MalformedEmailAddressException) as e:
+            extract_email_address_details('admin@mailserver1')
+            self.assertEqual('admin@mailserver1', e.exception.message)
+
+    def test_11_valid_email_address_top_level_domain(self):
+        expected = EmailDetails('example', 's.example')
+        actual = extract_email_address_details('example@s.example')
+        self.assertEqual(expected, actual)
+
+    def test_12_valid_email_address_spaces(self):
+        expected = EmailDetails('"  "', 'example.org')
+        actual = extract_email_address_details('"  "@example.org')
+        self.assertEqual(expected, actual)
+
+    def test_13_exception_raised_for_malformed_email_address(self):
         with self.assertRaises(MalformedEmailAddressException) as e:
             extract_email_address_details('BAD_EMAIL_ADDRESS')
             self.assertEqual('BAD_EMAIL_ADDRESS', e.exception.message)
 
-    def test_03_missing_domain(self):
+    def test_14_missing_domain(self):
         with self.assertRaises(MalformedEmailAddressException) as e:
             extract_email_address_details('sufi@')
             self.assertEqual('sufi@', e.exception.message)
 
-    def test_04_missing_id(self):
+    def test_15_missing_id(self):
         with self.assertRaises(MalformedEmailAddressException) as e:
             extract_email_address_details('@5yp.com')
             self.assertEqual('@5yp.com', e.exception.message)
 
-    def test_05_malformed_email(self):
+    def test_16_malformed_email(self):
         with self.assertRaises(MalformedEmailAddressException) as e:
             extract_email_address_details('bbb@xyz,com@')
             self.assertEqual('bbb@xyz,com@', e.exception.message)
 
-    def test_06_malformed_email(self):
+    def test_17_malformed_email(self):
         with self.assertRaises(MalformedEmailAddressException) as e:
             extract_email_address_details('@@@')
             self.assertEqual('@@@', e.exception.message)
 
-    def test_07_malformed_email(self):
+    def test_18_malformed_email(self):
         with self.assertRaises(MalformedEmailAddressException) as e:
             extract_email_address_details('abc @ 123 .... abc .... com')
             self.assertEqual('abc @ 123 .... abc .... com', e.exception.message)
