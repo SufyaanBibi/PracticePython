c2ecd6519c91420952353cd687c1edd89fd4d1b0
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-08 09:35:19 +0000

    Refactored BankAccount.py and accompanying tests.

--- CodingPractice/PythonAssignments/cleancode/BankAccount.py
+++ CodingPractice/PythonAssignments/cleancode/BankAccount.py
@@ -1,14 +1,28 @@
+from decimal import *
+
+
 class BankAccount:
 
     def __init__(self, acct_num, balance):
         self.acct_num = acct_num
         self.balance = balance
 
+    pence = Decimal('.01')
+
     def __add__(self, other):
-        return self.balance + other.balance
+        account_balance = Decimal(self.balance)
+        other_balance = Decimal(other.balance)
+        new_balance = account_balance + other_balance
+        return new_balance.quantize(self.pence, rounding=ROUND_HALF_UP)
 
     def __sub__(self, other):
-        return self.balance - other.balance
+        account_balance = Decimal(self.balance)
+        other_balance = Decimal(other.balance)
+        new_balance = account_balance - other_balance
+        return new_balance.quantize(self.pence, rounding=ROUND_HALF_UP)
 
-    def __mul__(self, other):
-        return self.balance * other
+    def __mul__(self, multiple):
+        balance = Decimal(self.balance)
+        multiplier = Decimal(multiple)
+        new_balance = balance * multiplier
+        return new_balance.quantize(self.pence, rounding=ROUND_HALF_UP)
--- CodingPractice/PythonAssignments/tests/cleancode/test_BankAccount.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_BankAccount.py
@@ -1,20 +1,34 @@
 import unittest
 from CodingPractice.PythonAssignments.cleancode.BankAccount import BankAccount
+from decimal import *
 
 
 class BankAccountTests(unittest.TestCase):
 
-    def test_can_add_bankAccounts(self):
+    def test_00_can_add_bank_accounts(self):
         a = BankAccount('basil', 100.11)
         b = BankAccount('sufi', 100.10)
-        self.assertEqual( 200.21, a + b)
+        self.assertEqual(Decimal('200.21'), a + b)
 
-    def test_can_subtract_bankAccounts(self):
+    def test_01_can_subtract_bank_accounts(self):
         a = BankAccount('basil', 100.50)
         b = BankAccount('sufi', 100.10)
-        self.assertEqual( 0.40, a - b)
+        self.assertEqual(Decimal('0.40'), a - b)
 
-    def test_can_get_1_percent_of_the_balance_in_a_bankAccount(self):
+    def test_02_can_get_1_percent_of_the_balance_in_a_bank_account(self):
         a = BankAccount('basil', 100.50)
-        self.assertEqual( 1.005, a * 0.01)
+        self.assertEqual(Decimal('1.01'), a * 0.01)
 
+    def test_03_can_add_int_bank_accounts(self):
+        a = BankAccount('basil', 100)
+        b = BankAccount('sufi', 100)
+        self.assertEqual(200, a + b)
+
+    def test_04_can_add_0_5(self):
+        a = BankAccount('basil', 0.25)
+        b = BankAccount('sufi', 0.25)
+        self.assertEqual(0.5, a + b)
+
+
+if __name__ == '__main__':
+    unittest.main()
