33adf4ee5dda3ece46a5da337fb4ac1f5894425c
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-08 13:07:19 +0000

    Refactored BankAccount.py and test_BankAccount.py as per request.

--- CodingPractice/PythonAssignments/cleancode/BankAccount.py
+++ CodingPractice/PythonAssignments/cleancode/BankAccount.py
@@ -5,7 +5,7 @@ class BankAccount:
 
     def __init__(self, acct_name, acct_balance):
         self._acct_name = acct_name
-        self._balance = Decimal(acct_balance)
+        self._balance = BankAccount.quantize(Decimal(acct_balance))
 
     pence = Decimal('.01')
 
@@ -13,14 +13,19 @@ class BankAccount:
     def quantize(deci):
         return deci.quantize(BankAccount.pence, rounding= ROUND_HALF_UP)
 
-    def __add__(self, other_acct):
-        new_balance = self._balance + other_acct._balance
-        return BankAccount.quantize(new_balance)
+    def __add__(self, ammount):
+        new_balance = self._balance + Decimal(ammount)
+        quantized = BankAccount.quantize(new_balance)
+        return BankAccount(self._acct_name, quantized)
 
-    def __sub__(self, other_acct):
-        new_balance = self._balance - other_acct._balance
-        return BankAccount.quantize(new_balance)
+    def __sub__(self, ammount):
+        new_balance = self._balance - Decimal(ammount)
+        quantized = BankAccount.quantize(new_balance)
+        return BankAccount(self._acct_name, quantized)
 
     def __mul__(self, multiple):
         new_balance = self._balance * Decimal(multiple)
         return BankAccount.quantize(new_balance)
+
+    def __eq__(self, other):
+        return self._balance == other._balance and self._acct_name == other._acct_name
