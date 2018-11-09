9c2892464909cddb9ecaf4f85d9a1263013f0ada
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-09 14:47:08 +0000

    Refactored DateUtils.py and accompanying tests.

--- CodingPractice/PythonAssignments/tests/cleancode/test_DateUtils.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_DateUtils.py
@@ -9,10 +9,25 @@ class DateCalculatorTests(unittest.TestCase):
         end = '2018/11/30'
         self.assertEqual(21, number_of_days_between(start, end))
 
-    def test_01_num_of_days_between(self):
+    def test_01_num_of_days_between_different_years(self):
         start = "1999/11/09"
         end = "2018/11/30"
         self.assertEqual(6961, number_of_days_between(start, end))
+
+    def test_02_num_of_days_between_different_months(self):
+        start = '1873/05/08'
+        end = '2018/11/09'
+        self.assertEqual(53145, number_of_days_between(start, end))
+
+    def test_03_same_day(self):
+        start = '2018/11/09'
+        end = '2018/11/09'
+        self.assertEqual(0, number_of_days_between(start, end))
+
+    def test_04_future_day_count(self):
+        start = '2018/12/25'
+        end = '2018/11/09'
+        self.assertEqual(-46, number_of_days_between(start, end))
 
 
 if __name__ == '__main__':
