9f272a84589cc25c4959c4ccec82e614dc1eed17
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-13 10:59:27 +0000

    Refactored UniqueCustomerName tests as per request.

--- CodingPractice/PythonAssignments/tests/test_UniqueCustomerName.py
+++ CodingPractice/PythonAssignments/tests/test_UniqueCustomerName.py
@@ -9,8 +9,8 @@ class UniqueFirstNameTests(unittest.TestCase):
         self.assertEqual(3, unique_customer_name(name_list))
 
     def test_01_names_with_upper_and_lower_letters_Alice_and_alice(self):
-        name_list = [('Alice', 'Wonderlund'), ('Sadie', 'Frost'), ('Sam', 'Antha'), ('Sadie', 'Lady'), ('alice', 'Joy')]
-        self.assertEqual(4, unique_customer_name(name_list))
+        name_list = [('Alice', 'Wonderlund'), ('alice', 'Joy')]
+        self.assertEqual(2, unique_customer_name(name_list))
 
 
 if __name__ == '__main__':
