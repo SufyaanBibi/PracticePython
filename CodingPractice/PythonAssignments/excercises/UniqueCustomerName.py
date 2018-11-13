e7c19a7c82d8ac20b9431a385603e2beb8569055
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-13 09:16:19 +0000

    Refactored variable in UniqueCustomerName.py.

--- CodingPractice/PythonAssignments/excercises/UniqueCustomerName.py
+++ CodingPractice/PythonAssignments/excercises/UniqueCustomerName.py
@@ -1,5 +1,5 @@
 
 def unique_customer_name(name_list):
-    unique_first_names = [name[0] for name in name_list]
-    first_names_set = set(unique_first_names)
+    first_names = [name[0] for name in name_list]
+    first_names_set = set(first_names)
     return len(first_names_set)
