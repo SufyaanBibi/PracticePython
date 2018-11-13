5107c5b9d87655373c6e16f1d755f5f2e8323f11
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-13 10:17:18 +0000

    Modified test_UniqueCustomerName.py as per request.

--- CodingPractice/PythonAssignments/tests/test_UniqueCustomerName.py
+++ CodingPractice/PythonAssignments/tests/test_UniqueCustomerName.py
@@ -5,10 +5,10 @@ from CodingPractice.PythonAssignments.excercises.UniqueCustomerName import uniqu
 class UniqueFirstNameTests(unittest.TestCase):
 
     def test_00_unique_first_names(self):
-        name_list = [ ('Alice', 'Wonderlund') , ('Sadie', 'Frost'), ('Sam', 'Antha'), ('Sadie', 'Lady') ]
+        name_list = [('Alice', 'Wonderlund'), ('Sadie', 'Frost'), ('Sam', 'Antha'), ('Sadie', 'Lady')]
         self.assertEqual(3, unique_customer_name(name_list))
 
-    def test_01_unique_names_with_lower_and_upper_cases(self):
+    def test_01_names_with_upper_and_lower_letters_Alice_and_alice(self):
         name_list = [('Alice', 'Wonderlund'), ('Sadie', 'Frost'), ('Sam', 'Antha'), ('Sadie', 'Lady'), ('alice', 'Joy')]
         self.assertEqual(4, unique_customer_name(name_list))
 
