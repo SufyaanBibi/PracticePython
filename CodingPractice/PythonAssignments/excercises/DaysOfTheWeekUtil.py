fccefa8611eea089b29234c8f6461e476f0500d3
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-13 10:07:42 +0000

    Started work on DaysOfTheWeekUtil.py

---
+++ CodingPractice/PythonAssignments/excercises/DaysOfTheWeekUtil.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def number_of_weekdays_between(start_date_str, end_date_str):
+    s_year, s_month, s_day = start_date_str.split('/')
+    e_year, e_month, e_day = end_date_str.split('/')
+    start_date = datetime(int(s_year), int(s_month), int(s_day))
+    end_date = datetime(int(e_year), int(e_month), int(e_day))
+
+    difference = end_date - start_date
+
+    return difference.days
