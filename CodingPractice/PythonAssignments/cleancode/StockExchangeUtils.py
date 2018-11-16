3a143dcf12bed389e6d90696172ccbd6f308abed
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-16 11:34:10 +0000

    Factored UncommonTickers.py and tests as per request.

--- CodingPractice/PythonAssignments/cleancode/StockExchangeUtils.py
+++ CodingPractice/PythonAssignments/cleancode/StockExchangeUtils.py
@@ -1,3 +1,7 @@
 
 def common_tickers(stock_exchange_1, stock_exchange_2):
     return stock_exchange_1.intersection(stock_exchange_2)
+
+
+def uncommon_tickers(stock_exchange_1, stock_exchange_2):
+    return stock_exchange_1.difference(stock_exchange_2)
