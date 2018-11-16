c526cf9cf0c5238bcaff3943a4c7684fbbc79f3c
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-16 10:12:43 +0000

    Refactored tests and CommonTickers.py.

--- CodingPractice/PythonAssignments/cleancode/CommonTickers.py
+++ CodingPractice/PythonAssignments/cleancode/CommonTickers.py
@@ -1,3 +1,3 @@
 
-def common_tickers(NYSE, LON):
-    return set(NYSE).intersection(LON)
+def common_tickers(stock_exchange_1, stock_exchange_2):
+    return stock_exchange_1.intersection(stock_exchange_2)
--- CodingPractice/PythonAssignments/tests/cleancode/test_CommonTickers.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_CommonTickers.py
@@ -9,6 +9,26 @@ class CommonTickersTests(unittest.TestCase):
         LON = {'APPL', 'IBM', 'LXE', 'AAC'}
         self.assertEqual({'APPL', 'IBM', 'AAC'}, common_tickers(NYSE, LON))
 
+    def test_01_uncommon_tickers(self):
+        NYSE = {'APP', 'IBB', 'ORCL', 'AAR', 'AAD'}
+        LON = {'APPL', 'IBM', 'LXE', 'AAC'}
+        self.assertEqual(set(), common_tickers(NYSE, LON))
+
+    def test_02_first_is_empty_set(self):
+        NYSE = set()
+        LON = {'APPL', 'IBM', 'LXE', 'AAC'}
+        self.assertEqual(set(), common_tickers(NYSE, LON))
+
+    def test_03_second_is_empty_set(self):
+        NYSE = {'APPL', 'IBM', 'ORCL', 'AAC', 'AAD'}
+        LON = set()
+        self.assertEqual(set(), common_tickers(NYSE, LON))
+
+    def test_04_both_empty(self):
+        NYSE = set()
+        LON = set()
+        self.assertEqual(set(), common_tickers(NYSE, LON))
+
 
 if __name__ == '__main__':
     unittest.main()
