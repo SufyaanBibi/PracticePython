dbd2960c630f7fab5bf56d84b011d1d2d0c3e9d0
Sufyaan <sufyaan.bibi@gmail.com>
2018-11-20 15:14:20 +0000

    StockExchangeUtils refactoring.

--- CodingPractice/PythonAssignments/cleancode/StockExchangeUtils.py
+++ CodingPractice/PythonAssignments/cleancode/StockExchangeUtils.py
@@ -5,3 +5,7 @@ def common_tickers(stock_exchange_1, stock_exchange_2):
 
 def uncommon_tickers(stock_exchange_1, stock_exchange_2):
     return stock_exchange_1.difference(stock_exchange_2)
+
+
+def all_tickers(stock_exchange_1, stock_exchange_2):
+    return stock_exchange_1.union(stock_exchange_2)
