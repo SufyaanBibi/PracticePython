20c01234b0b01ad54c0ded7679e32aa879b9588f
Sufyaan Bibi <sufyaan@Azura.connect>
2018-11-26 11:46:49 +0000

    Refactored is_lower_case's name.

--- CodingPractice/PythonAssignments/tests/cleancode/test_AllLowerCase.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_AllLowerCase.py
@@ -1,17 +1,17 @@
 import unittest
-from CodingPractice.PythonAssignments.cleancode.StringUtils import all_lower_case
+from CodingPractice.PythonAssignments.cleancode.StringUtils import is_lower_case
 
 
 class TestAllLowerCase(unittest.TestCase):
 
     def test_00_is_lower(self):
-        self.assertTrue(all_lower_case('ABcdE'))
+        self.assertTrue(is_lower_case('ABcdE'))
 
     def test_01_is_not_lower(self):
-        self.assertFalse(all_lower_case('ABCDE'))
+        self.assertFalse(is_lower_case('ABCDE'))
 
     def test_02_empty_string(self):
-        self.assertFalse(all_lower_case(''))
+        self.assertFalse(is_lower_case(''))
 
 
 if __name__ == '__main__':
