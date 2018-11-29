ab649f19b9d3e6a8607a7fc549fe8b835a38844a
Sufyaan Bibi <sufyaan@Azura.connect>
2018-11-29 16:43:00 +0000

    Refactored tests as per request.

--- CodingPractice/PythonAssignments/tests/cleancode/test_CustomerJsonDao.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_CustomerJsonDao.py
@@ -41,6 +41,25 @@ class TestCustomerJsonDao(unittest.TestCase):
         actual = c.get_customers_by_country('UK')
         self.assertEqual(expected, actual)
 
+    def test_04_idempotency_cust_id_v_iso_code(self):
+        expected = CustomerDetails(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
+                                   '11/25', 'UK')
+        c = CustomerJsonDao()
+        by_id = c.get_customer_by_id(101)
+        self.assertEqual(expected, by_id)
+        c1 = CustomerDetails(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
+                             '11/25', 'UK')
+        c2 = CustomerDetails(102, 'Charlie', 'Dogg', 'M', '4', '2015-06-19', 'snarly.dogg@burbage.rd.com',
+                             '11/30', 'UK')
+        c3 = CustomerDetails(104, 'Spooky', 'Dogg', 'M', '9', '2010-03-07', 'dodgy-paw@herne.hill.com',
+                             '08/14', 'UK')
+        expec = [c1, c2, c3]
+        c = CustomerJsonDao()
+        actual = c.get_customers_by_country('UK')
+        self.assertEqual(expec, actual)
+        by_id = c.get_customer_by_id(101)
+        self.assertEqual(expected, by_id)
+
 
 if __name__ == '__main__':
     unittest.main()
