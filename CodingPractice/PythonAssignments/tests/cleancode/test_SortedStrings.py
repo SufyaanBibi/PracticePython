be41603ac598043170c05ebcf620017f540c885e
Sufyaan Bibi <sufyaan@Azura.connect>
2018-11-29 10:42:28 +0000

    Added a new test to SortedStrings.py testing what happens when given a string with numbers in it.

--- CodingPractice/PythonAssignments/tests/cleancode/test_SortedStrings.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_SortedStrings.py
@@ -10,6 +10,9 @@ class TestSortedString(unittest.TestCase):
     def test_01_empty_string(self):
         self.assertEqual('', sorted_string(''))
 
+    def test_02_string_with_numbers(self):
+        self.assertEqual('123abzAX', sorted_string('a1bz3AX2'))
+
 
 if __name__ == '__main__':
     unittest.main()
