a5a526333772f9957042c43c246550daaf57f88a
Sufyaan Bibi <sufyaan.bibi@gmail.com>
2018-12-07 13:08:28 +0000

    Refactored code as per request, including variable name changes and function name changes.

--- CodingPractice/PythonAssignments/tests/cleancode/test_OrderAndOrderlineDto.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_OrderAndOrderlineDto.py
@@ -5,8 +5,8 @@ from CodingPractice.PythonAssignments.cleancode.OrderAndOrderlineDto import *
 class OrderDtoTest(unittest.TestCase):
 
     def test_00_construct_OrderDto(self):
-        a = OrderLineDto(1, 2, 3)
-        c = OrderLineDto(12, 21, 34)
+        a = OrderLineDto(order_id=1, product_id=2, qty=3)
+        c = OrderLineDto(order_id=12, product_id=21, qty=34)
         b = OrderDto(1, 101, "2018-11-25 11:45:15", [a, c])
         self.assertEqual(1, b.get_order_id())
         self.assertEqual(101, b.get_customer_id())
@@ -14,14 +14,14 @@ class OrderDtoTest(unittest.TestCase):
         self.assertEqual([a, c], b.get_order_lines())
 
     def test_01_equality_operator(self):
-        a = OrderLineDto(1, 2, 3)
+        a = OrderLineDto(order_id=1, product_id=2, qty=3)
         b = OrderDto(1, 101, "2018-11-25 11:45:15", [a])
-        c = OrderLineDto(1, 2, 3)
+        c = OrderLineDto(order_id=1, product_id=2, qty=3)
         d = OrderDto(1, 101, "2018-11-25 11:45:15", [c])
         self.assertEqual(b, d)
 
     def test_02_getter_functions_order_lines_dto(self):
-        a = OrderLineDto(1, 2, 3)
+        a = OrderLineDto(order_id=1, product_id=2, qty=3)
         self.assertEqual(1, a.get_order_id())
         self.assertEqual(2, a.get_product_id())
         self.assertEqual(3, a.get_qty())
--- CodingPractice/PythonAssignments/tests/cleancode/test_OrderJsonDao.py
+++ CodingPractice/PythonAssignments/tests/cleancode/test_OrderJsonDao.py
@@ -5,61 +5,61 @@ from CodingPractice.PythonAssignments.cleancode.OrderAndOrderlineDao import *
 class TestOrderJsonDao(unittest.TestCase):
 
     def test_00_get_orders(self):
-        o = OrderJsonDao()
-        e = o.get_orders()
+        odr = OrderJsonDao()
+        e = odr.get_orders()
         self.assertEqual(4, len(e))
 
     def test_01_get_order_by_order_id(self):
-        order_lines = OrderLineDto(7, 3, 10)
+        order_lines = OrderLineDto(order_id=7, product_id=3, qty=10)
         order = OrderDto(7, 103, "2018-12-01 10:45:15", [order_lines])
-        o = OrderJsonDao()
-        e = o.get_order_by_order_id(7)
+        odr = OrderJsonDao()
+        e = odr.get_order_by_order_id(7)
         self.assertEqual(order, e)
 
     def test_02_get_multiple_orders_by_cust_id(self):
-        order_lines_1 = OrderLineDto(1, 1, 1)
-        order_lines_2 = OrderLineDto(1, 2, 3)
+        order_lines_1 = OrderLineDto(order_id=1, product_id=1, qty=1)
+        order_lines_2 = OrderLineDto(order_id=1, product_id=2, qty=3)
         order_1 = OrderDto(1, 101, "2018-11-25 11:45:15", [order_lines_1, order_lines_2])
-        order_lines_3 = OrderLineDto(2, 1, 1)
-        order_lines_4 = OrderLineDto(2, 2, 3)
+        order_lines_3 = OrderLineDto(order_id=2, product_id=1, qty=1)
+        order_lines_4 = OrderLineDto(order_id=2, product_id=2, qty=3)
         order_2 = OrderDto(2, 101, "2018-11-30 11:45:15", [order_lines_3, order_lines_4])
-        o = OrderJsonDao()
-        e = o.get_orders_by_customer_id(101)
+        odr = OrderJsonDao()
+        e = odr.get_orders_by_customer_id(101)
         self.assertEqual([order_1, order_2], e)
 
     def test_03_get_single_order_by_cust_id(self):
-        order_lines = OrderLineDto(7, 3, 10)
+        order_lines = OrderLineDto(order_id=7, product_id=3, qty=10)
         order = OrderDto(7, 103, "2018-12-01 10:45:15", [order_lines])
-        o = OrderJsonDao()
-        e = o.get_orders_by_customer_id(103)
+        odr = OrderJsonDao()
+        e = odr.get_orders_by_customer_id(103)
         self.assertEqual([order], e)
 
     def test_03_get_order_by_product_id(self):
-        order_lines = OrderLineDto(7, 3, 10)
+        order_lines = OrderLineDto(order_id=7, product_id=3, qty=10)
         order = OrderDto(7, 103, "2018-12-01 10:45:15", [order_lines])
-        o = OrderJsonDao()
-        e = o.get_orders_by_product_id(3)
+        odr = OrderJsonDao()
+        e = odr.get_orders_by_product_id(3)
         self.assertEqual([order], e)
 
     def test_04_get_multiple_orders_by_product_id(self):
-        order_lines_1 = OrderLineDto(1, 1, 1)
-        order_lines_2 = OrderLineDto(1, 2, 3)
+        order_lines_1 = OrderLineDto(order_id=1, product_id=1, qty=1)
+        order_lines_2 = OrderLineDto(order_id=1, product_id=2, qty=3)
         order_1 = OrderDto(1, 101, "2018-11-25 11:45:15", [order_lines_1, order_lines_2])
-        order_lines_3 = OrderLineDto(2, 1, 1)
-        order_lines_4 = OrderLineDto(2, 2, 3)
+        order_lines_3 = OrderLineDto(order_id=2, product_id=1, qty=1)
+        order_lines_4 = OrderLineDto(order_id=2, product_id=2, qty=3)
         order_2 = OrderDto(2, 101, "2018-11-30 11:45:15", [order_lines_3, order_lines_4])
-        o = OrderJsonDao()
-        e = o.get_orders_by_product_id(1)
+        odr = OrderJsonDao()
+        e = odr.get_orders_by_product_id(1)
         self.assertEqual([order_1, order_2], e)
 
     def test_05_order_id_does_not_exist(self):
-        o = OrderJsonDao()
-        e = o.get_order_by_order_id(9000)
+        odr = OrderJsonDao()
+        e = odr.get_order_by_order_id(9000)
         self.assertEqual(None, e)
 
     def test_06_customer_id_does_not_exist(self):
-        o = OrderJsonDao()
-        e = o.get_orders_by_customer_id(9000)
+        odr = OrderJsonDao()
+        e = odr.get_orders_by_customer_id(9000)
         self.assertEqual(None, e)
 
 
