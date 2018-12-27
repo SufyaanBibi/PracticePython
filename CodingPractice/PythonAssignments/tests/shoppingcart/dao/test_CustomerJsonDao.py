b7cf881f862c21870a91faaa10059d1d0951ea80
Sufyaan Bibi <sufyaan.bibi@gmail.com>
2018-12-27 15:19:39 +0000

    Refactored Customer and Product DTO tests to use named parameters in constructors.

--- CodingPractice/PythonAssignments/tests/shoppingcart/dao/test_CustomerJsonDao.py
+++ CodingPractice/PythonAssignments/tests/shoppingcart/dao/test_CustomerJsonDao.py
@@ -5,23 +5,26 @@ from CodingPractice.PythonAssignments.shoppingcart.dao.CustomerJsonDao import *
 class TestCustomerJsonDao(unittest.TestCase):
 
     def test_00_get_customers(self):
-        expected = CustomerDto(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
-                            '11/25', 'UK')
+        expected = CustomerDto(customer_id=101, first_name='Spooky', last_name='Dogg', sex='M', age='10',
+                               birthday='2008-04-02', email_address='spooky.dogg@burbage.rd.com',
+                               mail_shot_date='11/25', iso_country_code='UK')
         c = CustomerJsonDao()
         actual = c.get_customers()
         self.assertEqual(expected, actual[0])
         self.assertEqual(4, len(actual))
 
     def test_01_get_cust_by_id(self):
-        expected = CustomerDto(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
-                            '11/25', 'UK')
+        expected = CustomerDto(customer_id=101, first_name='Spooky', last_name='Dogg', sex='M', age='10',
+                               birthday='2008-04-02', email_address='spooky.dogg@burbage.rd.com',
+                               mail_shot_date='11/25', iso_country_code='UK')
         c = CustomerJsonDao()
         actual = c.get_customer_by_id(101)
         self.assertEqual(expected, actual)
 
     def test_02_get_cust_by_first_last_name(self):
-        c1 = CustomerDto(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
-                            '11/25', 'UK')
+        c1 = CustomerDto(customer_id=101, first_name='Spooky', last_name='Dogg', sex='M', age='10',
+                               birthday='2008-04-02', email_address='spooky.dogg@burbage.rd.com',
+                               mail_shot_date='11/25', iso_country_code='UK')
         c2 = CustomerDto(104, 'Spooky', 'Dogg', 'M', '9', '2010-03-07', 'dodgy-paw@herne.hill.com',
                             '08/14', 'UK')
         expected = [c1, c2]
@@ -30,29 +33,36 @@ class TestCustomerJsonDao(unittest.TestCase):
         self.assertEqual(expected, actual)
 
     def test_03_get_cust_by_iso_country_code(self):
-        c1 = CustomerDto(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
-                            '11/25', 'UK')
-        c2 = CustomerDto(102, 'Charlie', 'Dogg', 'M', '4', '2015-06-19', 'snarly.dogg@burbage.rd.com',
-                            '11/30', 'UK')
-        c3 = CustomerDto(104, 'Spooky', 'Dogg', 'M', '9', '2010-03-07', 'dodgy-paw@herne.hill.com',
-                            '08/14', 'UK')
+        c1 = CustomerDto(customer_id=101, first_name='Spooky', last_name='Dogg', sex='M', age='10',
+                                birthday='2008-04-02', email_address='spooky.dogg@burbage.rd.com',
+                                mail_shot_date='11/25', iso_country_code='UK')
+        c2 = CustomerDto(customer_id=102, first_name='Charlie', last_name='Dogg', sex='M', age='4',
+                         birthday='2015-06-19', email_address='snarly.dogg@burbage.rd.com',
+                            mail_shot_date='11/30', iso_country_code='UK')
+        c3 = CustomerDto(customer_id=104, first_name='Spooky', last_name='Dogg', sex='M', age='9',
+                         birthday='2010-03-07', email_address='dodgy-paw@herne.hill.com',
+                            mail_shot_date='08/14', iso_country_code='UK')
         expected = [c1, c2, c3]
         c = CustomerJsonDao()
         actual = c.get_customers_by_iso_country_code('UK')
         self.assertEqual(expected, actual)
 
     def test_04_idempotency_cust_id_v_iso_code(self):
-        expected = CustomerDto(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
-                                   '11/25', 'UK')
+        expected = CustomerDto(customer_id=101, first_name='Spooky', last_name='Dogg', sex='M', age='10',
+                               birthday='2008-04-02', email_address='spooky.dogg@burbage.rd.com',
+                            mail_shot_date='11/25', iso_country_code='UK')
         c = CustomerJsonDao()
         by_id = c.get_customer_by_id(101)
         self.assertEqual(expected, by_id)
-        c1 = CustomerDto(101, 'Spooky', 'Dogg', 'M', '10', '2008-04-02', 'spooky.dogg@burbage.rd.com',
-                             '11/25', 'UK')
-        c2 = CustomerDto(102, 'Charlie', 'Dogg', 'M', '4', '2015-06-19', 'snarly.dogg@burbage.rd.com',
-                             '11/30', 'UK')
-        c3 = CustomerDto(104, 'Spooky', 'Dogg', 'M', '9', '2010-03-07', 'dodgy-paw@herne.hill.com',
-                             '08/14', 'UK')
+        c1 = CustomerDto(customer_id=101, first_name='Spooky', last_name='Dogg', sex='M', age='10',
+                               birthday='2008-04-02', email_address='spooky.dogg@burbage.rd.com',
+                            mail_shot_date='11/25', iso_country_code='UK')
+        c2 = CustomerDto(customer_id=102, first_name='Charlie', last_name='Dogg', sex='M', age='4',
+                         birthday='2015-06-19', email_address='snarly.dogg@burbage.rd.com',
+                            mail_shot_date='11/30', iso_country_code='UK')
+        c3 = CustomerDto(customer_id=104, first_name='Spooky', last_name='Dogg', sex='M', age='9',
+                         birthday='2010-03-07', email_address='dodgy-paw@herne.hill.com',
+                            mail_shot_date='08/14', iso_country_code='UK')
         expec = [c1, c2, c3]
         c = CustomerJsonDao()
         actual = c.get_customers_by_iso_country_code('UK')
