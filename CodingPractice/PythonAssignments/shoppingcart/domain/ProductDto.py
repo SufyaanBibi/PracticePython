556b5759c1bca3800b3b0800e0f909a9b373fc9b
Sufyaan Bibi <sufyaan.bibi@gmail.com>
2018-12-27 12:23:17 +0000

    Refactored code to reflect changes to the products.json.

--- CodingPractice/PythonAssignments/shoppingcart/domain/ProductDto.py
+++ CodingPractice/PythonAssignments/shoppingcart/domain/ProductDto.py
@@ -1,16 +1,18 @@
 
 class ProductDto:
 
-    def __init__(self, product_id, name, price, weight, stock_qty):
+    def __init__(self, product_id, name, price, weight, stock_qty, vatable):
         self._product_id = product_id
         self._name = name
         self._price = price
         self._weight = weight
         self._stock_qty = stock_qty
+        self._vatable = vatable
 
     def __eq__(self, other):
         return self._product_id == other._product_id and self._name == other._name and self._price == other._price \
-               and self._weight == other._weight and self._stock_qty == other._stock_qty
+               and self._weight == other._weight and self._stock_qty == other._stock_qty \
+               and self._vatable == other._vatable
 
     def get_id(self):
         return self._product_id
@@ -26,3 +28,6 @@ class ProductDto:
 
     def get_stock_qty(self):
         return self._stock_qty
+
+    def get_vatable(self):
+        return self._vatable
--- CodingPractice/PythonAssignments/tests/shoppingcart/dao/test_ProductJsonDao.py
+++ CodingPractice/PythonAssignments/tests/shoppingcart/dao/test_ProductJsonDao.py
@@ -5,42 +5,42 @@ from CodingPractice.PythonAssignments.shoppingcart.dao.ProductJsonDao import *
 class TestProductJsonDao(unittest.TestCase):
 
     def test_00_get_products(self):
-        expected = ProductDto(1, 'StandardWidget', 5.1, 100, 10000)
+        expected = ProductDto(1, 'StandardWidget', 5.1, 100, 10000, "True")
         p = ProductJsonDao()
         actual = p.get_products()
         self.assertEqual(expected, actual[0])
 
     def test_01_get_prod_by_id(self):
-        expected = ProductDto(1, 'StandardWidget', 5.1, 100, 10000)
+        expected = ProductDto(1, 'StandardWidget', 5.1, 100, 10000, "True")
         p = ProductJsonDao()
         actual = p.get_product_by_id(1)
         self.assertEqual(expected, actual)
 
     def test_02_get_prod_by_name(self):
-        expected = ProductDto(1, 'StandardWidget', 5.1, 100, 10000)
+        expected = ProductDto(1, 'StandardWidget', 5.1, 100, 10000, "True")
         p = ProductJsonDao()
         actual = p.get_products_by_name('StandardWidget')
         self.assertEqual(expected, actual[0])
 
     def test_03_get_prods_by_le_price(self):
-        p1 = ProductDto(1, 'StandardWidget', 5.1, 100, 10000)
-        p2 = ProductDto(4, "StandardGrommet", 5.1, 34, 10000)
+        p1 = ProductDto(1, 'StandardWidget', 5.1, 100, 10000, "True")
+        p2 = ProductDto(4, "StandardGrommet", 5.1, 34, 10000, "False")
         expected = [p1, p2]
         p = ProductJsonDao()
         actual = p.get_products_le_price(5.1)
         self.assertEqual(expected, actual)
 
     def test_04_get_prods_by_ge_price(self):
-        p1 = ProductDto(3, "SuperShinyWidget", 135.1, 150, 10)
-        p2 = ProductDto(6, "SuperShinyGrommet", 135.1, 1200, 10)
+        p1 = ProductDto(3, "SuperShinyWidget", 135.1, 150, 10, "True")
+        p2 = ProductDto(6, "SuperShinyGrommet", 135.1, 1200, 10, "True")
         expected = [p1, p2]
         p = ProductJsonDao()
         actual = p.get_products_ge_price(100)
         self.assertEqual(expected, actual)
 
     def test_05_get_prods_by_stock_qty(self):
-        p1 = ProductDto(3, "SuperShinyWidget", 135.1, 150, 10)
-        p2 = ProductDto(6, "SuperShinyGrommet", 135.1, 1200, 10)
+        p1 = ProductDto(3, "SuperShinyWidget", 135.1, 150, 10, "True")
+        p2 = ProductDto(6, "SuperShinyGrommet", 135.1, 1200, 10, "True")
         expected = [p1, p2]
         p = ProductJsonDao()
         actual = p.get_products_le_stock_qty(50)
