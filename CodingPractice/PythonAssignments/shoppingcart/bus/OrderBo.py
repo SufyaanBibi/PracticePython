373de6a7d83345b4d3e2218ea7fbb9127e9f02e7
Sufyaan Bibi <sufyaan.bibi@gmail.com>
2019-01-07 16:54:40 +0000

    Refactored OrderBo's postage methods to convert the postage matrix into a dictionary, so finding the postage rate is faster and more efficient.

--- CodingPractice/PythonAssignments/shoppingcart/bus/OrderBo.py
+++ CodingPractice/PythonAssignments/shoppingcart/bus/OrderBo.py
@@ -27,33 +27,34 @@ class OrderBo:
         self._order_dao = order_dao
         self._product_dao = product_dao
         self._cust_dao = cust_dao
-        self._postage_matrix = self.convert_postage_matrix(postage_matrix)
+        self._postage_matrix = self._new_postage_matrix(postage_matrix)
 
     @staticmethod
-    def convert_postage_matrix(postage_matrix):
-        result = []
+    def _new_postage_matrix(postage_matrix):
+        postage_dict = {}
         for country, weight, postage_class, price in postage_matrix:
             if weight == '1kg' and postage_class == '1st Class':
                 new_weight = 1000
                 new_postage_class = 1
-                t = (country, new_weight, new_postage_class, price)
-                result.append(t)
+                t = (country, new_weight, new_postage_class)
+                postage_dict[t] = price
             elif weight == '1kg' and postage_class == '2nd Class':
                 new_weight = 1000
                 new_postage_class = 2
-                t = (country, new_weight, new_postage_class, price)
-                result.append(t)
+                t = (country, new_weight, new_postage_class)
+                postage_dict[t] = price
             elif weight == '2kg' and postage_class == '1st Class':
                 new_weight = 2000
                 new_postage_class = 1
-                t = (country, new_weight, new_postage_class, price)
-                result.append(t)
+                t = (country, new_weight, new_postage_class)
+                postage_dict[t] = price
             elif weight == '2kg' and postage_class == '2nd Class':
                 new_weight = 2000
                 new_postage_class = 2
-                t = (country, new_weight, new_postage_class, price)
-                result.append(t)
-        return result
+                t = (country, new_weight, new_postage_class)
+                postage_dict[t] = price
+        return postage_dict
+
 
     pence = Decimal('.01')
 
@@ -143,7 +144,5 @@ class OrderBo:
             weight = 1000
         elif weight > 1000:
             weight = 2000
-        for c_c, w, p_c, rate in self._postage_matrix:
-            if c_c == iso_country_code and w == weight and p_c == postage_class:
-                return rate
-
+        key = (iso_country_code, weight, postage_class)
+        return self._postage_matrix[key]
